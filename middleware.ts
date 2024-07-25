b51d4da449ffbc255cb79cf2a7c5eab8c79e80fb
Karthikeyan Balasubramanian <jkarthionline@gmail.com>
2024-07-25 21:59:51 +0530

    formatted all the files

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,5 @@ import type { NextRequest } from "next/server";
 import { getUserMeLoader } from "./data/services/get-user-me-loader";
 
 export async function middleware(request: NextRequest) {
-  const user = await getUserMeLoader();
-  const currentPath = request.nextUrl.pathname;
-
-  if (currentPath.startsWith("/dashboard") && user.ok === false) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
-  }
-
   return NextResponse.next();
 }
