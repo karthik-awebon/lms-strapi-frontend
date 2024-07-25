a6cb5c640f3c04ecbeffa10a7962ec93700e14fb
Karthikeyan Balasubramanian <jkarthionline@gmail.com>
2024-07-26 00:19:42 +0530

    debugging added to login

--- data/actions/auth-actions.ts
+++ data/actions/auth-actions.ts
@@ -117,7 +117,7 @@ export async function loginUserAction(prevState: any, formData: FormData) {
       message: "Failed to Login.",
     };
   }
-
+  console.log('cookies', config);
   cookies().set("jwt", responseData.jwt, config);
 
   redirect("/");
